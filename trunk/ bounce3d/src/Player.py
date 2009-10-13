2d9e876df7eca755cf32b431e9e7601170376ca7
latenssi <latenssi@dfa05384-b715-11de-997e-c5eb2a8ae348>
2009-10-13 19:39:57 +0000

    Lisäsin Player-luokan. Muutin liikkeen kontrolloinnin event-pohjaiseksi. Ball-luokan jump metodi pitäisi implementoida. Huom! KeyboardControl-luokan jump metodiin liittyvä kommentointi!

---
+++ trunk/ bounce3d/src/Player.py
@@ -0,0 +1,44 @@
+from direct.showbase.ShowBase import ShowBase
+
+from Event import Event, createNamedEvent
+
+class Player:
+
+  NAME_DEFAULT = "unnamed" 
+  RIGHT_DEFAULT = False
+  LEFT_DEFAULT = False
+
+  def __init__(self, name = NAME_DEFAULT):
+    self.name = name
+    self.right = Player.RIGHT_DEFAULT
+    self.left = Player.LEFT_DEFAULT
+
+  def jump(self):
+    jumpEvent = createNamedEvent(self.name, Event.PLAYER_JUMP)
+    messenger.send(jumpEvent)
+
+  def moveRightOn(self):
+    self.right= True
+    moveEvent = createNamedEvent(self.name, Event.PLAYER_MOVE_RIGHT_ON)
+    messenger.send(moveEvent)
+
+  def moveRightOff(self):
+    self.right = False
+    moveEvent = createNamedEvent(self.name, Event.PLAYER_MOVE_RIGHT_OFF)
+    messenger.send(moveEvent)
+
+  def isRightOn(self):
+    return self.right
+
+  def moveLeftOn(self):
+    self.left = True
+    moveEvent = createNamedEvent(self.name, Event.PLAYER_MOVE_LEFT_ON)
+    messenger.send(moveEvent)
+
+  def moveLeftOff(self):
+    self.left = False
+    moveEvent = createNamedEvent(self.name, Event.PLAYER_MOVE_LEFT_OFF)
+    messenger.send(moveEvent)
+
+  def isLeftOn(self):
+    return self.left

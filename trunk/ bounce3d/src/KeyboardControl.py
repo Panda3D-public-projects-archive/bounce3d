40e374f02353a1fd7b95bef10621e67a77f58c3a
tohlosormi <tohlosormi@dfa05384-b715-11de-997e-c5eb2a8ae348>
2009-10-29 08:20:32 +0000

    Altered controls. Change is still in progress.

--- trunk/ bounce3d/src/KeyboardControl.py
+++ trunk/ bounce3d/src/KeyboardControl.py
@@ -10,6 +10,8 @@ class KeyboardControl(DirectObject):
 
 	PLAYER_RIGHT_KEY = "arrow_right"
 	PLAYER_LEFT_KEY = "arrow_left"
+	PLAYER_UP_KEY = "arrow_up"
+	PLAYER_DOWN_KEY = "arrow_down"
 	PLAYER_JUMP = "space"
 	TURN_GRAVITY = "g"
 	TURN_GRAVITY2 = "h"
@@ -44,10 +46,14 @@ class KeyboardControl(DirectObject):
 		self.accept(playerJumpOn, ball.jumpOn) 	
 		self.accept(playerJumpOff, ball.jumpOff)
 
-		self.accept(KeyboardControl.PLAYER_RIGHT_KEY, player.moveRightOn)
-		self.accept(KeyboardControl.PLAYER_RIGHT_KEY + "-up", player.moveRightOff)
-		self.accept(KeyboardControl.PLAYER_LEFT_KEY, player.moveLeftOn)
-		self.accept(KeyboardControl.PLAYER_LEFT_KEY + "-up", player.moveLeftOff)
+		self.accept(KeyboardControl.PLAYER_RIGHT_KEY, ball.arrowRightDown)
+		self.accept(KeyboardControl.PLAYER_RIGHT_KEY + "-up", ball.arrowRightUp)
+		self.accept(KeyboardControl.PLAYER_LEFT_KEY, ball.arrowLeftDown)
+		self.accept(KeyboardControl.PLAYER_LEFT_KEY + "-up", ball.arrowLeftUp)
+		self.accept(KeyboardControl.PLAYER_UP_KEY, ball.arrowUpDown)
+		self.accept(KeyboardControl.PLAYER_UP_KEY + "-up", ball.arrowUpUp)
+		self.accept(KeyboardControl.PLAYER_DOWN_KEY, ball.arrowDownDown)
+		self.accept(KeyboardControl.PLAYER_DOWN_KEY + "-up", ball.arrowDownUp)
 		self.accept(KeyboardControl.PLAYER_JUMP, player.jumpOn)
 		self.accept(KeyboardControl.PLAYER_JUMP + "-up", player.jumpOff)
 		

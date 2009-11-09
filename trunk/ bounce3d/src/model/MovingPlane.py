886f881c40cf47652abc9e47466d3d12db7d8fbd
teemu.linkosaari@utu.fi <teemu.linkosaari@utu.fi@dfa05384-b715-11de-997e-c5eb2a8ae348>
2009-11-09 19:18:22 +0000

    Exit-laatikko

--- trunk/ bounce3d/src/model/MovingPlane.py
+++ trunk/ bounce3d/src/model/MovingPlane.py
@@ -1,21 +1,42 @@
 
 from pandac.PandaModules import (
-	Quat,OdeBody, OdeMass, OdeBoxGeom, BitMask32)
+        Quat,OdeBody, OdeMass, OdeBoxGeom, BitMask32)
+from math import sin
 
 class MovingPlane:
-	def __init__(self, space, pos):
-		# Hauska Bugi: kirjoita 1.0 eika 1
-		lx,ly,lz = 5,5,1.0   # dimension
-		px,py,pz = pos # position
-	
-		self.ground = loader.loadModel("box")		
-		self.ground.setScale( lx, ly, lz )
-		self.ground.setPos( px -lx/2, py -ly/2, pz -lz/2)
-		
-		self.ground.flattenLight()
-		self.ground.reparentTo(render)
+        ''' moving plane '''
+        id = 0
+        
+        def __init__(self, space, pos, dim):
+		''' Hauska Bugi: kirjoita 1.0 eika 1 '''
+                MovingPlane.id += 1
+                self.id = id
+                self.dim = dim # dimension
+                self.pos = pos # position
+                self.h = ( dim[0]/2, dim[1]/2, dim[2]/2 )
+                self.t = 0
 
-		geom = OdeBoxGeom( space, lx, ly, lz)
-		geom.setPosition( px, py, pz)
+                self.geom = OdeBoxGeom( space, dim[0], dim[1], dim[2])
 
+                self.model = loader.loadModel("box")
+                self.model.setScale( dim[0], dim[1], dim[2] )
+                self.model.flattenLight()
+                self.model.reparentTo(render)
+
+                self.updateModelNode()
+
+        def updateModelNode(self):
+                self.t = self.t + 0.03
+                pos = self.pos
+                h = self.h
+
+                rot = pos[2] + sin( self.t )
+
+                self.geom.setPosition( pos[0], pos[1], rot)
+                self.model.setPos(pos[0]-h[0], pos[1]-h[1], rot-h[2])
+
+	def getId(self):
+		return id;
 	
+	def getGeom(self):
+		return self.geom

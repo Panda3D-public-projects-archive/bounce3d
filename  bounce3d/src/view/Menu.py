3a3d07cfca8ff224a4354f2c1234ac015d782eb1
qwertyjelly <qwertyjelly>
2009-11-30 20:51:32 +0000

    Menu -class redone to support multiple submenues. Not yet working properly

---  bounce3d/src/view/Menu.py
+++  bounce3d/src/view/Menu.py
@@ -10,67 +10,93 @@ from event.EventType import EventType
 
 class Menu:
 	
-	
-	OPTIONS = ["Start","Next level"]
-	# Should contain Event_Type :s
-	OPTION_EVENTS = ["RESTART","NEXT_LEVEL"]
-	SELECTION_POINTERS = ["-[","]-"]
-	DEFAULT_APPEND = ""
+	DEFAULT_SELECTION_POINTERS = ["-[", "]-"]
 	# BACKGROUND_IMAGE ="../media/bg_test.tif"
 	DEFAULT_SELECTION = -1
+	DEFAULT_VISIBILITY = False
 	
-	def __init__(self, menu_text = OPTIONS, default = DEFAULT_SELECTION):
-		self.menu_text = menu_text
+	def __init__(
+		self,
+		engine,
+		info_texts,
+		item_texts,
+		item_events,
+		selection = DEFAULT_SELECTION,
+		visibility = DEFAULT_VISIBILITY
+	):
+		self.engine=engine
+		self.info_texts = info_texts
+		self.info_table = []
+		self.item_texts = item_texts
 		self.menu_items = []
-		self.selection = default
+		self.item_events = item_events
+		self.selection = selection
+		self.visible = visibility
+
+	def generateMenu(self):
 		
-		
-		# Generates a menu.
-		for i in self.menu_text:
+		for i in self.info_texts:
 			item = OnscreenText(
 				text = i,
 				style = 1,
 				fg = (1,1,1,1),
-				pos = (0,0-(self.menu_text.index(i))*0.1),
+				pos = (0,(((len(self.info_texts) + len(self.item_texts))*0.5) -(self.info_texts.index(i))*0.1)),
+				align = TextNode.ACenter,
+				scale = 0.07
+			)
+			item.hide()
+			self.info_table.append(item)
+			
+		for i in self.item_texts:
+			item = OnscreenText(
+				text = i,
+				style = 1,
+				fg = (1,1,1,1),
+				pos = (0,(((len(self.info_texts) + len(self.item_texts))*0.5) -(self.item_texts.index(i))*0.1)),
 				align = TextNode.ACenter,
 				scale = 0.07
 			)
 			item.hide()
 			self.menu_items.append(item)
 		self.selectionDown()
-	
+
 	def selectionUp(self):
 		if ( 0 < self.selection ):
-			self.menu_items[self.selection].setText(self.OPTIONS[self.selection])
-			self.menu_items[self.selection-1].setText(self.SELECTION_POINTERS[0] + self.OPTIONS[self.selection-1] + self.SELECTION_POINTERS[1])
+			self.menu_items[self.selection].setText(self.item_texts[self.selection])
+			self.menu_items[self.selection-1].setText(self.DEFAULT_SELECTION_POINTERS[0] + self.item_texts[self.selection-1] + self.DEFAULT_SELECTION_POINTERS[1])
 			self.selection -= 1
 		else:
 			print "Menu top reached"
 
 	def selectionDown(self):
-		if ( self.selection < (len(self.OPTIONS)-1) ):
-			self.menu_items[self.selection].setText(self.OPTIONS[self.selection])
-			self.menu_items[self.selection+1].setText(self.SELECTION_POINTERS[0] + self.OPTIONS[self.selection+1] + self.SELECTION_POINTERS[1])
+		if ( self.selection < (len(self.item_texts)-1) ):
+			self.menu_items[self.selection].setText(self.item_texts[self.selection])
+			self.menu_items[self.selection+1].setText(self.DEFAULT_SELECTION_POINTERS[0] + self.item_texts[self.selection+1] + self.DEFAULT_SELECTION_POINTERS[1])
 			self.selection += 1
 		else:
 			print "Menu bottom reached"
 
 	def select(self):
-		'''
-		for i in self.OPTIONS:
-			if self.selection == self.OPTIONS.index(i):
-				messenger.send(EventType.self.OPTION_EVENTS[self.OPTIONS.index(i)]) # Work in progress
-		'''
-		print "Selection made"
 		self.hideMenu()
+		messenger.send(self.item_events[self.selection])
+		messenger.send(EventType.CONTROL_CHANGE)
+		print "Selection"
 
 	def showMenu(self):
-		messenger.send(EventType.CONTROL_CHANGE)
-		for i in self.menu_items:
-			i.show()
+		if not self.visible:
+			self.visible = True
+			messenger.send(EventType.CONTROL_CHANGE)
+			for i in self.menu_items:
+				i.show()
+			for j in self.info_table:
+				j.show()
 
 	# Should only be called when a selection is made
 	def hideMenu(self):
-		messenger.send(EventType.CONTROL_CHANGE)
-		for i in self.menu_items:
-			i.hide()
+		if self.visible:
+			self.visible = False
+			messenger.send(EventType.CONTROL_CHANGE)
+			for i in self.menu_items:
+				i.hide()
+			for j in self.info_table:
+				j.hide()

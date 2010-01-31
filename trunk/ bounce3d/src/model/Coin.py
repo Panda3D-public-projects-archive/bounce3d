a9f89815a95bcaccb8b8f044c227ffab6866811c
tohlosormi <tohlosormi@dfa05384-b715-11de-997e-c5eb2a8ae348>
2010-01-31 18:26:18 +0000

    Floating coins god dammit.

--- trunk/ bounce3d/src/model/Coin.py
+++ trunk/ bounce3d/src/model/Coin.py
@@ -1,6 +1,10 @@
 
 from pandac.PandaModules import (
 	Quat,OdeBody, OdeMass, OdeBoxGeom, BitMask32)
+	
+from pandac.PandaModules import (CollisionTraverser,
+	CollisionSphere, CollisionNode, NodePath,
+	PandaNode, CollisionHandlerEvent)
 
 from model.SurfaceType import SurfaceType
 
@@ -8,12 +12,15 @@ class Coin:
 	"""
 	An object that a ball collects.
 	"""
-	MODEL = "box.egg"
+	MODEL = "../egg/kolikko.egg"
 	collectable = 0
 
-	def __init__(self, world, space, pos):
+	def __init__(self, model, world, space, pos):
+		self.model = model
 		self.world = world
 		self.space = space
+		self.collHandEvent = CollisionHandlerEvent()
+		self.collHandEvent.addInPattern('into-%in')
 		self.addBox(pos)
 		self.isCollected = False
 		Coin.collectable += 1
@@ -24,10 +31,17 @@ class Coin:
 			self.box.setColor(1,0,0)
 			Coin.collectable -= 1
 
+	def onCollide(self, entry):
+		body1 = entry.getFromNodePath()
+		ballNode = self.model.getBall().getModelNode()
+		print ballNode
+		if ballNode and ballNode == body1.getParent():
+			self.collect()
+
 	def addBox(self, pos):
 		lx,ly,lz = 1,1,1   # dimension
 		px,py,pz = pos # position
-		
+		name = "box" + str(pos)
 		self.box = loader.loadModel(self.MODEL)
 		self.box.setPos(-0.5,-0.5,-0.5)
 		self.box.flattenLight() # ApplyTransform
@@ -38,9 +52,25 @@ class Coin:
 		self.box.setScale( lx, ly, lz )
 		self.box.setHpr( 0, 50, 0 )
 		
+		# Offset z by -1.0 because the coin model was so small
+		cSphere = CollisionSphere(pos[0], pos[1], pos[2] - 1.0, 1)
+		cNode = CollisionNode(name)
+		cNode.addSolid(cSphere)
+		
+		model = render.find(name)
+		if not model.isEmpty():
+			model.removeNode()
+			
+		cnodePath = render.attachNewNode( cNode )
+		#cnodePath.show()
+		base.cTrav.addCollider( cnodePath, self.collHandEvent )
+		base.accept( 'into-' + name, self.onCollide )
+		
+		# Implementation below would not allow coins to float?
 		# define mass
+		"""
 		mass = OdeMass()
-		mass.setBox( 500, lx, ly, lz)
+		mass.setBox(500, lx, ly, lz)
 		
 		self.boxBody = OdeBody( self.world )
 		self.boxBody.setPosition( self.box.getPos(render) )
@@ -50,13 +80,17 @@ class Coin:
 		self.geom = OdeBoxGeom( self.space, lx, ly, lz)
 		self.space.setSurfaceType( self.geom, SurfaceType.COIN )
 		self.geom.setBody( self.boxBody )
-	
+		self.boxBody = None
+		"""
 	def updateModelNode(self):
-		self.box.setPos( render, self.boxBody.getPosition() )
-		self.box.setQuat(render, Quat(self.boxBody.getQuaternion() ) )
+		return None
+		#None
+		#self.box.setPos( render, self.boxBody.getPosition() )
+		#self.box.setQuat(render, Quat(self.boxBody.getQuaternion() ) )
 	
 	def getBody(self):
-		return self.boxBody
+		return None
+		#return self.boxBody
 	
 	def removeNode(self):
 		if not self.isCollected:
